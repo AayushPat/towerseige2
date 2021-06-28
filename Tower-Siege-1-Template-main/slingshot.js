d124dc7d1b88930ea8b1114d688454eea027fbb3
AayushPat <patelaayush12@yahoo.com>
2021-06-28 11:52:33 -0700

    towersiegecomplete

---
+++ Tower-Siege-1-Template-main/slingshot.js
@@ -0,0 +1,28 @@
+class SlingShot {
+    constructor(point, objectA) {
+        var options = {
+            bodyA: objectA,
+            pointB: point,
+            stiffness: 0.4,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+
+    }
+    fly() {
+        this.sling.bodyA = null;
+    }
+    display() {
+        if (this.sling.bodyA) {
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.sling.pointB;
+            push()
+            strokeWeight(4); fill("white");
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            pop()
+        }
+
+    }
+
+}
